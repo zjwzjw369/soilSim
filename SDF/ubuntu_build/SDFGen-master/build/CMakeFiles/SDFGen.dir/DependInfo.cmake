c30da2390b262232ab1c2baf21001c070c138066
geekzzz <820760596@qq.com>
2018-12-22 15:32:43 +0800

    add ubuntu SDFGen

---
+++ SDF/ubuntu_build/SDFGen-master/build/CMakeFiles/SDFGen.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zhanghaitong/Desktop/SDFGen-master/main.cpp" "/home/zhanghaitong/Desktop/SDFGen-master/build/CMakeFiles/SDFGen.dir/main.cpp.o"
+  "/home/zhanghaitong/Desktop/SDFGen-master/makelevelset3.cpp" "/home/zhanghaitong/Desktop/SDFGen-master/build/CMakeFiles/SDFGen.dir/makelevelset3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "H5_BUILT_AS_DYNAMIC_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_SQL_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/vtk-8.2"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/include"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/include/QtWidgets"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/include/QtGui"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/include/QtCore"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/./mkspecs/linux-g++"
+  "/home/zhanghaitong/Qt/5.12.0/gcc_64/include/QtSql"
+  "/usr/local/include/vtk-8.2/vtkogg/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SDF/ubuntu_build/SDFGen-master/build/CMakeFiles/SDFGen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDFGen.dir/main.cpp.o"
+  "CMakeFiles/SDFGen.dir/makelevelset3.cpp.o"
+  "bin/SDFGen.pdb"
+  "bin/SDFGen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDFGen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
